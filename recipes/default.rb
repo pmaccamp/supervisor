22e5e4d80a0227cd9390c7a78c2527aeef4ac93f
Patrick McKeown <pmaccamp@sbcglobal.net>
2018-12-07 13:43:47 -0800

    Remove python dependencies

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,8 +17,6 @@
 # limitations under the License.
 #
 
-include_recipe "python"
-
 # foodcritic FC023: we prefer not having the resource on non-smartos
 if platform_family?("smartos")
   package "py27-expat" do
@@ -26,9 +24,12 @@ if platform_family?("smartos")
   end
 end
 
-python_pip "supervisor" do
-  action :upgrade
-  version node['supervisor']['version'] if node['supervisor']['version']
+if node['supervisor']['version']
+  execute "pip install -y supervisor==#{node['supervisor']['version']}" do
+  end
+else
+  execute "pip install -y supervisor" do
+  end
 end
 
 directory node['supervisor']['dir'] do
